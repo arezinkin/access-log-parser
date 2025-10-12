999d30820a14e18089a9150afffe1314ee722cb0
arezinkin <arezin@yandex.ru>
2025-10-12 16:12:44 +0300

    Программа опять полностью переработана: задание про типы данных внесено, арфметические операции с двумя числами пользователя - теперь ее главное назначение.
    Старый код больше точно не нужен.

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>: ");
+        // Это не по заданию, а для души. А то почти все воскресенье учусь.
+        System.out.println(" ");
+        System.out.println("   -= Привет! Добро пожаловать в программу работы с числами! =-");
+        System.out.println("           Это важный шаг в жизни любой программы!");
+        System.out.println(" ");
 
-        // Считываем строку из командной строки
-        String text = new Scanner(System.in).nextLine();
+        System.out.println(" !!! Пожалуйста, введите ПЕРВОЕ целое число и нажмите <Enter>: ");
+        // TODO: явно нужно сделать проверку на вводимый тип данных, если ввести не число, то программа падает по ошибке. Может exception поставить? но нас пока не учили как :(
+        int input_number1 = new Scanner(System.in).nextInt();
 
-        System.out.println("Длина текста: " + text.length());
+        System.out.println(" !!! Введите ВТОРОЕ целое число и нажмите <Enter>: ");
+        // TODO: явно нужно сделать проверку на вводимый тип данных, если ввести не число, то программа падает по ошибке. Может exception поставить? но нас пока не учили как :(
+        int input_number2 = new Scanner(System.in).nextInt();
 
+        System.out.println(" - сумма введённых чисел: " + (input_number1+input_number2)
+                            + " . Формула: " + input_number1 + "+" + input_number2);
+
+        System.out.println(" - разность введённых чисел: " + (input_number1-input_number2)
+                + " . Формула: " + input_number1 + "-" + input_number2);
+
+        System.out.println(" - произведение9 введённых чисел: " + (input_number1*input_number2)
+                + " . Формула: " + input_number1 + "*" + input_number2);
+
+        double chastnoe = (double) input_number1/input_number2;
+
+        if (input_number2!=0) {
+            System.out.println(" - частное введённых чисел: " + chastnoe
+                    + " . Формула: " + input_number1 + "/" + input_number2);
+        } else {
+            System.out.println(" - частное введённых чисел: Второе введённое число = 0, операция деления не доступна!");
+        }
 
     }
 

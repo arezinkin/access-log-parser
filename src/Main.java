27fb699bba1ff8a5c30cb55672bdf9ccf9d8f12f
arezinkin <arezin@yandex.ru>
2025-10-26 20:02:14 +0300

    Вводим имена файлов и проверяем что это вообще файлы. Сразу в мастер (так как я один разработчик проекта - могу себе позволить)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,13 +6,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>: ");
+        int count_good_files = 0;
 
-        // Считываем строку из командной строки
-        String text = new Scanner(System.in).nextLine();
+        do {
+            System.out.println(" Пожалуйста, укажите путь к файлу для парсинга:");
 
-        System.out.println("Длина текста: " + text.length());
+            String path = new Scanner(System.in).nextLine();
 
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean fileIsDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println(" ^");
+                System.out.println(" !!! Ошибка файла: введенная строка не ведет к существущему файлу!" );
+                System.out.println(" ===");
+                continue;
+            }
+
+            if (fileIsDirectory) {
+                System.out.println(" !!! Ошибка файла: указанный путь ведет к папке, а не файлу!");
+                System.out.println("     ^");
+                continue;
+            }
+
+            // до сюда доходят корректные пути к файлу, можем путь сохранять и счетчик увеличивать
+            //todo: кажется нужно проверять, а не дубль ли это ранее указанного файла, и не увеличивать счетчик, если дубль
+            count_good_files++;
+
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count_good_files);
+            System.out.println(" ===");
+
+        } while (true);
 
     }
 
